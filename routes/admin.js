e5bca4f3282ee906a82cc9613281a6112d98a68f
faizanthecoder <faizanthecoder@gmail.com>
2026-01-07 16:16:00 +0500

    .

--- routes/admin.js
+++ routes/admin.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const router = express.Router();
 const Product = require("../models/Product");
 const Order = require("../models/Order");
