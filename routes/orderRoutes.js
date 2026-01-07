b07324a1dd5482b3989c81c6a68b5f8d84128b74
faizanthecoder <faizanthecoder@gmail.com>
2026-01-07 13:42:43 +0500

    .

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 const Order = require("../models/Order");
 
-router.post("/", async (req, res) => {
-  const order = new Order(req.body);
-  await order.save();
-  res.json({ success: true });
+router.get("/", async (req, res) => {
+  const orders = await Order.find().sort({ createdAt: -1 });
+  res.json(orders);
 });
+
 
 module.exports = router;
